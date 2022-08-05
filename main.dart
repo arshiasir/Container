901dad6c7c43208213931f4a717f7bccf1efd2d0
arshia_sir <98395557+arshiasir@users.noreply.github.com>
2022-08-05 09:29:31 -0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
