bbc64e90c623094113e4f8a5b1493d61b62ce01a
arshia_sir <98395557+arshiasir@users.noreply.github.com>
2022-08-05 09:33:01 -0800

    Update home.dart

--- home.dart
+++ home.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,42 +8,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  double numberOffset  = 0;
-  double numberblurRadius = 0;
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-      Duration(seconds: 1) ,(){
-        setState(() {
-          numberOffset =20;
-          numberblurRadius =20;
-        });
-      }
-    );
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 86, 86, 86),
       body: Center(
-        child:AnimatedContainer(
-          duration: Duration(seconds: 5),
-          width: 200,
-          height: 200,
+        child: Container(
+          width: 400,
+          height: 400,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
             color: Color.fromARGB(255, 86, 86, 86),
             boxShadow: [
               BoxShadow(
                 color: Color.fromARGB(255, 197, 197, 197),
-                offset: Offset(-numberOffset, -numberOffset),
-                blurRadius: numberblurRadius,
+                offset: Offset(-20, -20),
+                blurRadius: 30,
               ),
               BoxShadow(
                 color: Color.fromARGB(255, 20, 20, 20),
-                offset: Offset(numberOffset, numberOffset),
-                blurRadius: numberblurRadius,
+                offset: Offset(20, 20),
+                blurRadius: 30,
               ),
             ],
           ),
